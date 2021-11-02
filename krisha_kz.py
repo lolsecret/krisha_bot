94b688ef12c47e08d4d77050a105cceed0ab5b96
Tamerlan <tsabyrbek@globerce.com>
2021-11-02 16:45:17 +0600

    апдейт

--- krisha_kz.py
+++ krisha_kz.py
@@ -7,7 +7,7 @@ from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 
 HOST = 'https://krisha.kz'
-URL = 'https://krisha.kz/arenda/kvartiry/almaty/?das[live.rooms]=1&das[rent.period]=2'
+URL = "https://krisha.kz/prodazha/kvartiry/almaty/?areas=p43.263887%2C76.928178%2C43.259496%2C76.908093%2C43.257112%2C76.894532%2C43.257614%2C76.884061%2C43.252219%2C76.865006%2C43.239168%2C76.857711%2C43.237976%2C76.857453%2C43.235529%2C76.859170%2C43.227747%2C76.862603%2C43.223353%2C76.865865%2C43.220089%2C76.866466%2C43.214188%2C76.883546%2C43.214314%2C76.902600%2C43.215193%2C76.932469%2C43.224734%2C76.942082%2C43.226366%2C76.964398%2C43.238415%2C76.956674%2C43.245443%2C76.971608%2C43.274299%2C76.970235%2C43.264013%2C76.927835%2C43.263887%2C76.928178&das[_sys.hasphoto]=1&das[flat.building]=1&das[floor_not_first]=1&das[floor_not_last]=1&das[house.year][from]=1980&das[live.rooms][]=2&das[live.rooms][]=3&das[live.rooms][]=4&das[live.rooms][]=5.100&das[price][to]=40000000&lat=43.24358&lon=76.90966&zoom=13"
 HEADERS = {
     'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36'
 }
@@ -25,18 +25,70 @@ def get_content(html):
     houses = []
     for item in items:
         img = item.find('picture', class_='is-moderated')
-        houses.append({
+        main_data = {
             'title': item.find('a', class_='a-card__title').get_text(),
             'subtitle': item.find('div', class_='a-card__subtitle').get_text(strip=True),
-            'preview': item.find('div', class_='a-card__text-preview').get_text(strip=True),
             'owner': item.find('div', class_='owners__label').get_text(strip=True),
             'created_at': ''.join([it.get_text(strip=True) for it in item.find_all('div', class_='card-stats__item')][1:2]),
             'link': HOST + item.find('a', class_='a-card__title').get('href'),
             'price': item.find('div', class_='a-card__price').get_text(strip=True).replace(u'\xa0', ' '),
             'image': img.get('data-full-src') if hasattr(img, 'class') else img_not_found,
             'href': item.find('a').get('href')
-        })
+        }
+
+        url = HOST + item.find('a', class_='a-card__title').get('href')
+        additional_data = get_images(url)
+        main_data.update(additional_data)
+        houses.append(main_data)
     return houses
+
+
+def get_images(url):
+    html = get_html(url)
+    print(url)
+    images = []
+    soup = BeautifulSoup(html.content, features="html.parser")
+    try:
+        imgs = soup.find('div', class_="gallery__container").find('ul', class_="gallery__small-list").find_all('li')
+        for i in imgs:
+            img = i.img['src']
+            if img:
+                r = img.rfind("-")
+                lst = list(img)
+                lst[r:] = "-full.webp"
+                lst = "".join(lst)
+                images.append(lst)
+
+    except AttributeError:
+        ...
+    if len(images) < 1:
+        main_picture =  soup.find('div', class_="gallery__main").picture.img['src']
+        images.append(main_picture)
+        images.append(main_picture)
+    building = soup.find('div', {'data-name': 'flat.building'}).find('div', class_='offer__advert-short-info').get_text()
+    balcony = soup.find('div', {'data-name': 'flat.balcony'}).find('div', class_='offer__advert-short-info').get_text() if soup.find('div', {'data-name': 'flat.balcony'}) else ''
+    square =  soup.find('div', {'data-name': 'live.square'}).find('div', class_='offer__advert-short-info').get_text()
+    try:
+        preview = soup.find('div', class_="offer__description").find('div', class_="text").div.get_text(strip=True).replace('\n', '')
+    except AttributeError:
+        preview = ''
+    additional_data = {
+        "square": square,
+        "balcony":  balcony,
+        "images": images,
+        "preview": preview,
+        "building": building,
+    }
+    return additional_data
+
+def get_phone(url):
+    from selenium import webdriver
+    driver = webdriver.Chrome("/home/blank/tamerlan_projects/selenium_projects/chromedriver")
+    driver.get(url)
+    driver.find_element_by_class_name('show-phones').click()
+    val = driver.find_element_by_class_name("offer__contacts-phones").text()
+    driver.quit()
+    return val
 
 
 def download_image(url):
@@ -48,9 +100,11 @@ def download_image(url):
     return filename
 
 
-# def parse_href(href):
-#     result = re.match(f'/show/{href}')
-#     return result.group(1)
+def remove_image(url):
+    a = urlparse(url)
+    filename = os.path.basename(a.path)
+    os.remove(filename)
+    return
 
 
 def update_lastkey(new_key, lastkey_file):
@@ -87,8 +141,9 @@ def parse(lastkey_file=False):
     # for page in range(1, last_page):
         # print(f'Парсинг страницы #{page} из {last_page}')
     html = get_html(URL)
-    print('ya tut')
     houses = get_content(html.content)
+
+
 
     # Скачиваем картинку
     # download_image(img)
--- telegram_bot.py
+++ telegram_bot.py
@@ -1,11 +1,14 @@
 import asyncio
+import json
+
+import aiogram.types.input_media
 
 import config
 import logging
 
 from aiogram import Bot, Dispatcher, executor, types
 from sql_db import SQL
-from krisha_kz import new_appartments, download_image, update_lastkey, parse
+from krisha_kz import new_appartments, download_image, update_lastkey, parse, remove_image
 
 logging.basicConfig(level=logging.INFO)
 
@@ -27,7 +30,7 @@ async def subscribe(message: types.Message):
 
 # Команда отписки
 @dp.message_handler(commands=['subscribe'])
-async def subscribe(message: types.Message):
+async def unsubscribe(message: types.Message):
     if not db.subscriber_exists(message.from_user.id):
         # если юзера нет в базе, добавляем его
         db.add_subscriber(message.from_user.id, False)
@@ -39,38 +42,80 @@ async def subscribe(message: types.Message):
     await message.answer("Вы успешно подписались на рассылку \nЖдите, скоро выйдут новые объявления")
 
 
+@dp.message_handler(commands=['region'])
+async def region(message: types.Message):
+    btn_1 = types.InlineKeyboardButton("Алмалинский", callback_data='btn_ALM')
+    btn_2 = types.InlineKeyboardButton("Медеуский", callback_data='btn_MED')
+    btn_3 = types.InlineKeyboardButton("Бостандыкский", callback_data='btn_BOS')
+    kb = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    kb.add(btn_1).add(btn_2).add(btn_3)
+    await message.reply("Выберите район: ", reply_markup=kb)
+
+
+@dp.callback_query_handler(text_contains='btn_')
+async def process_callback_kb1btn1(callback_query: types.CallbackQuery):
+    print(callback_query)
+    code = callback_query.data[-1]
+    print(code)
+    if code == "ALM":
+        await bot.answer_callback_query(callback_query.id, text='Нажата вторая кнопка')
+    elif code == "MED":
+        await bot.answer_callback_query(
+            callback_query.id,
+            text='Нажата кнопка с номером 5.\nА этот текст может быть длиной до 200 символов 😉', show_alert=True)
+    else:
+        await bot.answer_callback_query(callback_query.id)
+    await bot.send_message(callback_query.from_user.id, f'Нажата инлайн кнопка! code={code}')
+
 # проверяем наличие новых игр и делаем рассылки
 async def scheduled(wait_for):
+
     while True:
         await asyncio.sleep(wait_for)
 
-        # проверяем наличие новых игр
+        # проверяем наличие новых кв
         new_houses = new_appartments('lastkey.txt')
         if new_houses:
             # если объявления есть, переворачиваем список и итерируем
             # получаем список подписчиков бота
             subscriptions = db.get_subscriptions()
             # отправляем всем новость
-            with open(download_image(new_houses['image']), 'rb') as photo:
-                for s in subscriptions:
-                    await bot.send_photo(
+
+            print('start')
+
+            for s in subscriptions:
+                media = types.MediaGroup()
+                for i in new_houses["images"][:9]:
+                    media.attach_photo(types.InputFile(download_image(i)))
+                bot_msg = await bot.send_photo(
                     s[1],
-                    photo,
-                    caption=new_houses['title'] + "\n\n" + "*Адрес*: " + new_houses['subtitle'] + "\n" +
-                    "*Описание*: " + new_houses['preview'] + "\n\n" 
-                    "*Хозяин/специалист*: " + new_houses['owner'] + "\n"
-                    "*Цена*: " + new_houses['price'] + "\n\n"
-                    "*Ссылка*: " + new_houses['link'] + "\n" +
-                    "*Опубликован*: " + new_houses['created_at'] + "\n\n"
+                    types.InputFile(download_image(new_houses['image'])),
+                    caption=new_houses['title'] + "\n\n"
+                        "<b>Адрес:</b> " + new_houses['subtitle'] + "\n"
+                        "<b>Дом:</b> " + new_houses['building'] +"\n"
+                        "<b>Балкон:</b> " + new_houses['balcony'] +"\n"
+                        "<b>Площадь:</b> " + new_houses['square'] + "\n"
+                        "<b>Описание:</b> " + new_houses['preview'] + "\n\n"
+                        "<b>Хозяин/специалист:</b> " + new_houses['owner'] + "\n"
+                        "<b>Цена:</b> " + new_houses['price'] + "\n\n"
+                        "<b>Ссылка:</b> " + new_houses['link'] + "\n" +
+                        "<b>Опубликован:</b> " + new_houses['created_at'] + "\n\n"
                     ,
                     disable_notification=True,
-                    parse_mode='Markdown'
-                    )
+                    parse_mode='html'
+                )
+                await bot.send_media_group(
+                    s[1],
+                    media=media,
+                    reply_to_message_id=bot_msg.message_id
+                )
+
             update_lastkey(new_houses['href'].split("/")[3], 'lastkey.txt')
-
-
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
-    loop.create_task(scheduled(300))
+    loop.create_task(scheduled(60))
     executor.start_polling(dp, skip_updates=True)
+
+
+
